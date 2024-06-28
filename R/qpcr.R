9640ef04bd9ad5dd16311739b5be7b3f1a1f44e5
Rodrigo Aluizio <r.aluizio@gmail.com>
2024-06-28 10:48:53 -0300

    Initial commit

---
+++ R/qpcr.R
@@ -0,0 +1,12 @@
+#' Auxiliary table for rq package, used in examples
+#'
+#' @format ## `qpcr`
+#' A tibble with 45 rows and 4 columns:
+#' \describe{
+#'   \item{Sample}{Sample names}
+#'   \item{ITS2}{ITS2 locus Cq data}
+#'   \item{pUC18}{pUC18 plasmid data}
+#'   \item{DCq}{Delta Cq between ITS2 and pUC18}
+#' }
+#' @source artificial data
+"qpcr"
---
+++ R/qpcr_rq.r
@@ -0,0 +1,107 @@
+#' @title
+#' Calculates the Relative Quantity of a qPCR set
+#'
+#' @description
+#' It takes a tibble of five columns: Sample, Target Name, Normalizer Name
+#' and DCq (Delta Cq), to calculate the relative quantity and return another
+#' tibble containing such data. Take a look at qpcr data available with this
+#' package for and example.
+#'
+#' @param data A tibble
+#' @param normalizer The name of the normalizer samples.
+#' @param reference The name of the reference target.
+#' @param treatment The name of the treatment target.
+#' @param delimiter The delimiter character separating sample name and replica. Defaults to "-".
+#' @return A tibble
+#' @importFrom glue glue
+#' @importFrom dplyr summarise group_by n
+#' @importFrom tidyr separate_wider_delim pivot_longer
+#' @importFrom cli cli_alert cli_alert_danger bg_red
+#' @importFrom stats sd
+#' @importFrom tidyselect all_of
+#' @export
+#' @examples
+#' rq(qpcr, "pUC18", "WT", "ITS2")
+rq <- function(data, normalizer, reference, treatment,
+                    delimiter = '-') {
+  Sample <- Cq <- DCq <- SD <- NULL
+  if (is.null(normalizer)) {
+    stop(bg_red('You must provide the normalizer samples name.'),
+         call. = F)
+  }
+  if (is.null(reference)) {
+    stop(bg_red('You must provide the reference target name.'),
+         call. = F)
+  }
+  if (is.null(treatment)) {
+    stop(bg_red('You must provide the treatment target name.'),
+         call. = F)
+  }
+  # tidy the data to a format suitable for analyses
+  tryCatch(
+    {
+      cat("\n")
+      cli_alert("Tidying data...")
+      qpcr <- pivot_longer(qpcr, cols = all_of(c(treatment, normalizer)),
+                           names_to = 'Target', values_to = 'Cq')
+    },
+    error = function(cond) {
+      cat("\n")
+      cli_alert_danger(bg_red("Error tidying data."))
+      stop(conditionMessage(cond), call. = F)
+    }
+  )
+  # Sample means ignoring normalizer data
+  cli_alert("Calculating table means...")
+  qpcr_mean <- qpcr[qpcr$Target != normalizer,] |>
+    group_by(Sample) |>
+    summarise(
+      `Mean Cq` = mean(`Cq`, na.rm = T),
+      `Mean DCq` = mean(`DCq`, na.rm = T),
+      .groups = "keep"
+    )
+  ## Sample Names Adjustment
+  tryCatch(
+    {
+      cli_alert("Adjusting sample naming...")
+      qpcr_mean <- qpcr_mean |>
+        separate_wider_delim(
+          Sample, delim = delimiter,
+          names = c(
+            "Sample", "Replica"
+          )
+        )
+    },
+    error = function(cond) {
+      cat("\n")
+      cli_alert_danger(bg_red("Error adjusting sample names."))
+      stop(conditionMessage(cond), call. = F)
+    }
+  )
+  ## Defining Base Value
+  one <- mean(qpcr_mean$`Mean DCq`[
+    qpcr_mean$Sample == reference
+  ])
+  if (is.na(one)) {
+    cli_alert_danger(bg_red("Error with reference."))
+    stop(reference, call. = F)
+  }
+  ## Relative Quantity
+  cli_alert("Calculating Relative Quantity...")
+  qpcr_mean$RQ <- 2^-(qpcr_mean$`Mean DCq` - (one))
+
+  ## Mean Relative Quantity
+  cli_alert("Calculating mean, sd and se of Relative Quantity...")
+  RQ <- qpcr_mean |>
+    group_by(Sample) |>
+    summarise(
+      `Mean RQ` = mean(RQ, na.rm = T),
+      `SD` = sd(RQ, na.rm = T),
+      `SE` = `SD`/ sqrt(n())
+    )
+  ## Set the reference value
+  cli_alert("Setting comparison value to 1...")
+  RQ$`Mean RQ`[RQ$Sample == reference] <- 1
+
+  return(RQ)
+}
